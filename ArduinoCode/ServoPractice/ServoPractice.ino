85e0f7afc1a6aa8f73fd6a12323ef9ac15638cfc
kaiyuanlee3641 <kaiyuanlee@umass.edu>
2022-04-26 15:48:08 -0400

    Update ServoPractice.ino with simple movement

--- ArduinoCode/ServoPractice/ServoPractice.ino
+++ ArduinoCode/ServoPractice/ServoPractice.ino
@@ -1,21 +1,45 @@
 #include <ESP32Servo.h>
 #define POT A0
 
-Servo myservo1; 
-Servo myservo2; 
-Servo myservo3;
-Servo myservo4;
+Servo servo1; 
+Servo servo2; 
+Servo servo3;
+Servo servo4;
 void setup() {
-  myservo1.attach(18);
-  myservo2.attach(23);
-  myservo3.attach(15); 
-  myservo4.attach(2);
+  servo1.attach(23);
+  servo2.attach(18);
+  servo3.attach(15); 
+  servo4.attach(2);
+  
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
 }
 
+int moveDegree = 45;
 void loop() {
-  myservo1.write(analogRead(A0)/4095.0*180);
-  myservo2.write(analogRead(A0)/4095.0*180);
-  myservo3.write(analogRead(A0)/4095.0*180);
-  myservo4.write(analogRead(A0)/4095.0*180);
-  delay(5);
+  for(int i=90;i<=90+moveDegree;i++){
+    Serial.print(i);
+    delay(10);
+    servo1.write(i);
+    servo3.write(i);
+    
+    servo2.write(90-moveDegree+(i-90));
+    servo4.write(90-moveDegree+(i-90));
+  }
+  for(int i=90+moveDegree;i>=90;i--){
+
+    Serial.print(i);
+    delay(10);
+    servo1.write(i);
+    servo3.write(i);
+    
+    servo2.write(90-(90+moveDegree-i));
+    servo4.write(90-(90+moveDegree-i));
+  }
+//  servo1.write(analogRead(A0)/4095.0*180);
+//  servo2.write(analogRead(A0)/4095.0*180);
+//  servo3.write(analogRead(A0)/4095.0*180);
+//  servo4.write(analogRead(A0)/4095.0*180);
 }
