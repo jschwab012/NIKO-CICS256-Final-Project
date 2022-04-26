ca032e7034a33e5fbf249af654803509169c06bb
kaiyuanlee3641 <kaiyuanlee@umass.edu>
2022-04-26 17:04:37 -0400

    Improved movement so it moves forward

--- ArduinoCode/ServoPractice/ServoPractice.ino
+++ ArduinoCode/ServoPractice/ServoPractice.ino
@@ -6,6 +6,7 @@ Servo servo2;
 Servo servo3;
 Servo servo4;
 void setup() {
+  Serial.begin(115200);
   servo1.attach(23);
   servo2.attach(18);
   servo3.attach(15); 
@@ -18,28 +19,52 @@ void setup() {
 }
 
 int moveDegree = 45;
+int leftL = 90;
+int leftF = 90;
+int rightL = 90-moveDegree;
+int rightF = 90-moveDegree;
+int leftV = 1;
+int rightV = 1;
 void loop() {
-  for(int i=90;i<=90+moveDegree;i++){
-    Serial.print(i);
-    delay(10);
-    servo1.write(i);
-    servo3.write(i);
-    
-    servo2.write(90-moveDegree+(i-90));
-    servo4.write(90-moveDegree+(i-90));
+  leftL = constrain(leftL, 90, 90+moveDegree);
+  leftF = constrain(leftF, 90, 90+moveDegree);
+  rightL = constrain(rightL, 90-moveDegree, 90);
+  rightF = constrain(rightF, 90-moveDegree, 90);
+  leftL += leftV;
+  if(leftV < 0){
+    leftF += leftV*2;
   }
-  for(int i=90+moveDegree;i>=90;i--){
+  else{
+    leftF += leftV;
+  }
+  
+  rightL += rightV;
+  if(rightV > 0){
+    rightF += rightV*2;
+  }
+  else{
+    rightF += rightV;
+  }
+  if(leftL == 90+moveDegree || leftL == 90){
+    leftV = -leftV;
+  }
+  if(rightL == 90-moveDegree || rightL == 90){
+    rightV = -rightV;
+  }
 
-    Serial.print(i);
-    delay(10);
-    servo1.write(i);
-    servo3.write(i);
-    
-    servo2.write(90-(90+moveDegree-i));
-    servo4.write(90-(90+moveDegree-i));
-  }
-//  servo1.write(analogRead(A0)/4095.0*180);
-//  servo2.write(analogRead(A0)/4095.0*180);
-//  servo3.write(analogRead(A0)/4095.0*180);
-//  servo4.write(analogRead(A0)/4095.0*180);
+  servo1.write(leftL);
+  servo2.write(rightL);
+  servo3.write(leftF);
+  servo4.write(rightF);
+  Serial.print(leftV);
+  Serial.print(" ");
+  Serial.println(rightV);
+  Serial.print(leftL);
+  Serial.print(" ");
+  Serial.println(leftF);
+  Serial.print(rightL);
+  Serial.print(" ");
+  Serial.println(rightF);
+  Serial.println("----");
+  delay(5);
 }
