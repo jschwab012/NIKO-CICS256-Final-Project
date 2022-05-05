0217f7bf9c833441f68b43e88f23e3514d701502
Joseph Schwab <jschwab@umass.edu>
2022-05-05 14:37:30 -0400

    Added mp3

--- ArduinoCode/Main/MP3.cpp
+++ ArduinoCode/Main/MP3.cpp
@@ -1,1 +1,17 @@
+#include "MP3.h"
 
+// Connections for serial interface to the YX5300 module
+const uint8_t ARDUINO_RX = 15;    // connect to TX of MP3 Player module
+const uint8_t ARDUINO_TX = 13;    // connect to RX of MP3 Player module
+
+SoftwareSerial  MP3Stream(ARDUINO_RX, ARDUINO_TX);
+MD_YX5300 mp3(MP3Stream);
+
+void initMP3(){
+  MP3Stream.begin(MD_YX5300::SERIAL_BPS);
+  mp3.begin();
+}
+
+void playMP3(uint8_t trackNum){
+  mp3.playTrack(trackNum);
+}
--- ArduinoCode/Main/MP3.h
+++ ArduinoCode/Main/MP3.h
@@ -1,1 +1,19 @@
+#ifndef MP3_H
+#define MP3_H
 
+//Include libraries
+#include <MD_YX5300.h>
+#include <SoftwareSerial.h>
+
+// Connections for serial interface to the YX5300 module
+extern const uint8_t ARDUINO_RX;    // connect to TX of MP3 Player module
+extern const uint8_t ARDUINO_TX;    // connect to RX of MP3 Player module
+
+extern SoftwareSerial  MP3Stream;
+extern MD_YX5300 mp3;
+
+//Functions
+void initMp3();
+void playMP3(uint8_t trackNum);
+
+#endif
